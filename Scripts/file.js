77b69f3c3eac290517397ce538392e0cb6d87a56
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-07-24 20:46:13 +0300

    change title

--- Scripts/file.js
+++ Scripts/file.js
@@ -41,8 +41,7 @@ $(".loading-overlay .sk-cube-grid").fadeOut(1000,
 		function(){	
 	$("body").css("overflow","auto");
 	$(this).parent().fadeOut(2000,
-		 function(){
-		
+		function(){
 		$(this).remove();
 		});
 	});	
